fd66b879d139aea5744c4a4016314f33e194805b
rfaircloth-splunk <rfaircloth@splunk.com>
2021-11-20 16:14:58 -0500

    fix: remove certain files from code output

--- scripts/index.sh
+++ scripts/index.sh
@@ -14,7 +14,7 @@ echo from inside index.sh
 grep -Po 'ModuleIdentity\(\(((?:\d(?:, )?)*)\)\)' output/notexts/* \
 	| sed 's|.*/notexts/||' \
 	| sed 's|, |.|g' \
-	| sed 's|.py:ModuleIdentity((|,|' \
+	| sed 's|*:ModuleIdentity((|,|' \
 	| sed 's|))||' >output/index.csv
 
 while IFS=, read -r one two; do 
@@ -23,3 +23,37 @@ while IFS=, read -r one two; do
     echo $one>>output/index/$d/mib.txt
     echo $d, $one    
 done <output/index.csv
+
+rm -f output/texts/SNMPv2-MIB*
+rm -f output/texts/INET-ADDRESS-MIB*
+rm -f output/texts/PYSNMP-USM-MIB*
+rm -f output/texts/RFC-1212*
+rm -f output/texts/RFC-1215*
+rm -f output/texts/RFC1065-SMI*
+rm -f output/texts/RFC1155-SMI*
+rm -f output/texts/RFC1158-MIB*
+rm -f output/texts/RFC1213-MIB*
+rm -f output/texts/SNMP-FRAMEWORK-MIB*
+rm -f output/texts/SNMP-TARGET-MIB*
+rm -f output/texts/SNMPv2-CONF*
+rm -f output/texts/SNMPv2-SMI*
+rm -f output/texts/SNMPv2-TC*
+rm -f output/texts/SNMPv2-TM*
+rm -f output/texts/TRANSPORT-ADDRESS-MIB*
+
+rm -f output/notexts/SNMPv2-MIB*
+rm -f output/notexts/INET-ADDRESS-MIB*
+rm -f output/notexts/PYSNMP-USM-MIB*
+rm -f output/notexts/RFC-1212*
+rm -f output/notexts/RFC-1215*
+rm -f output/notexts/RFC1065-SMI*
+rm -f output/notexts/RFC1155-SMI*
+rm -f output/notexts/RFC1158-MIB*
+rm -f output/notexts/RFC1213-MIB*
+rm -f output/notexts/SNMP-FRAMEWORK-MIB*
+rm -f output/notexts/SNMP-TARGET-MIB*
+rm -f output/notexts/SNMPv2-CONF*
+rm -f output/notexts/SNMPv2-SMI*
+rm -f output/notexts/SNMPv2-TC*
+rm -f output/notexts/SNMPv2-TM*
+rm -f output/notexts/TRANSPORT-ADDRESS-MIB*
