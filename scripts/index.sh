234732f3b55263bca25c62b198bdb6316632626c
rfaircloth-splunk <rfaircloth@splunk.com>
2021-11-20 11:37:34 -0500

    feat:  Include index.csv

--- scripts/index.sh
+++ scripts/index.sh
@@ -15,11 +15,11 @@ grep -Po 'ModuleIdentity\(\(((?:\d(?:, )?)*)\)\)' output/notexts/* \
 	| sed 's|.*/notexts/||' \
 	| sed 's|, |.|g' \
 	| sed 's|.py:ModuleIdentity((|,|' \
-	| sed 's|))||' >index.csv
+	| sed 's|))||' >output/index.csv
 
 while IFS=, read -r one two; do 
     d=$(echo $two | sed 's|\.|/|g')
     mkdir -p output/index/$d
     echo $one>>output/index/$d/mib.txt
     echo $d, $one    
-done <index.csv
+done <output/index.csv
