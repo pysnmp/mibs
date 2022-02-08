92bc8b1e340f8689631c0eb521d22648184c9e10
rfaircloth-splunk <rfaircloth@splunk.com>
2022-02-08 17:00:35 -0500

    fix: index notification types

--- index.py
+++ index.py
@@ -13,17 +13,23 @@ for filename in os.listdir(p):
             if "class" in data:
                 classname = data["class"]
                 if classname in ["moduleidentity", "objectidentity"]:
-                    # print(f"{classname} {data['oid']}")
-                    if data and (
-                        data["class"] == "moduleidentity"
-                        or data["class"] == "objectidentity"
+                    mapped = True
+                    index[data["oid"]] = filename.replace(".json", "")
+        if not mapped:
+            for i in jmib.items():
+                data = i[1]
+                if "class" in data:
+                    classname = data["class"]
+                    if (
+                        classname not in ["moduleidentity", "objectidentity"]
+                        and "oid" in data
                     ):
                         mapped = True
                         index[data["oid"]] = filename.replace(".json", "")
+
         if not mapped and "-TC" not in filename:
             print(f"Unable to index {p}/{filename}")
 
 with open("output/index.csv", "w") as f:
     for i in index.items():
-
         f.write(f"{i[1]},{i[0]}\n")
