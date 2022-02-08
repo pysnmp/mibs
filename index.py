693a81ef37cf7c360f4911e9288fb93a25a17edd
rfaircloth-splunk <rfaircloth@splunk.com>
2022-02-08 16:48:55 -0500

    fix: updates
    * Remove NETWORK-APPLIANCE-MIB obsoleted by NETAPP-MIB
    * improve index solution

--- index.py
+++ index.py
@@ -5,13 +5,23 @@ import os
 p = "output/json"
 index = {}
 for filename in os.listdir(p):
+    mapped = False
     with open(os.path.join(p, filename), "r") as read_file:
         jmib = json.load(read_file)
         for i in jmib.items():
             data = i[1]
-            if "class" in data and data["class"] == "moduleidentity":
-                index[data["oid"]] = filename.replace(".json", "")
-                break
+            if "class" in data:
+                classname = data["class"]
+                if classname in ["moduleidentity", "objectidentity"]:
+                    # print(f"{classname} {data['oid']}")
+                    if data and (
+                        data["class"] == "moduleidentity"
+                        or data["class"] == "objectidentity"
+                    ):
+                        mapped = True
+                        index[data["oid"]] = filename.replace(".json", "")
+        if not mapped and "-TC" not in filename:
+            print(f"Unable to index {p}/{filename}")
 
 with open("output/index.csv", "w") as f:
     for i in index.items():
