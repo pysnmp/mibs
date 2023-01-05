9bc42c9feb447ce2dbc16a733b0933de578e1f60
omrozowicz-splunk <omrozowicz@splunk.com>
2023-01-05 10:49:19 +0100

    add log with all successfully compiled mibs

--- local_mibs.sh
+++ local_mibs.sh
@@ -6,6 +6,8 @@ then
   source /tmp/.cache/pysnmp-mibs*/bin/activate
   echo "Found local mibs. Compiling..."
   make index-local-mibs
+  echo "Successfully compiled MIBs are: "
+  sed 's/,.*//g' index.csv | uniq | sort
   # The convention here is to have no MIB extension (like .my or .mib) so we cut it off
   find /tmp/new_mibs/output/asn1 -type f -name '*.*' | while read f; do mv "$f" "${f%.*}"; done
   echo "Preparing new mibs to be served..."
